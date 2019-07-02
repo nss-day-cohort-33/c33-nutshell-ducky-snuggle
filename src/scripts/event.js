24b8027aa0be5e18ce7ef86472c50f19d9d354c5
Shane Miller <shanemiller89@gmail.com>
2019-07-01 21:14:22 -0500

    refactor of event listeners into methods

--- src/scripts/event.js
+++ src/scripts/event.js
@@ -5,31 +5,59 @@ import { registerUserForm } from "./login_register.js";
 
 let targetContainer = document.querySelector("#container");
 
-function submitRegBtn() {
-  document.querySelector("#submit-reg-btn").addEventListener("click", () => {
-    console.log("you clicked the save");
-    let userName = document.querySelector("#userName").value;
-    let email = document.querySelector("#email").value;
-    let userObj = utilityFunc.createUserObj(userName, email);
-    console.log(userObj);
-    API.saveToApi("user", userObj);
-    targetContainer = "";
-  });
-}
+const EVENT = {
+  loginUserBtn: function () {
+    document.querySelector("#login-btn").addEventListener("click", () => {
+      let userName = document.querySelector("#login").value;
+      console.log(userName);
+    });
+  },
+  registerPageLink: function () {
+    document.querySelector("#register-link").addEventListener("click", () => {
+      event.preventDefault();
+      targetContainer.innerHTML = registerUserForm();
+      EVENT.submitRegBtn();
+    });
+  },
+  submitRegBtn: function () {
+    document.querySelector("#submit-reg-btn").addEventListener("click", () => {
+      console.log("you clicked the save");
+      let userName = document.querySelector("#userName").value;
+      let email = document.querySelector("#email").value;
+      let userObj = utilityFunc.createUserObj(userName, email);
+      console.log(userObj);
+      API.saveToApi("user", userObj);
+      // targetContainer.innerHTML = ""; --Will clear container upon submit--
+    });
+  }
+};
 
-function registerPageLink() {
-  document.querySelector("#register-link").addEventListener("click", () => {
-    event.preventDefault();
-    targetContainer.innerHTML = registerUserForm();
-    submitRegBtn();
-  });
-}
 
-function loginUserBtn() {
-  document.querySelector("#login-btn").addEventListener("click", () => {
-    let userName = document.querySelector("#login").value;
-    console.log(userName);
-  });
-}
+// function submitRegBtn() {
+//   document.querySelector("#submit-reg-btn").addEventListener("click", () => {
+//     console.log("you clicked the save");
+//     let userName = document.querySelector("#userName").value;
+//     let email = document.querySelector("#email").value;
+//     let userObj = utilityFunc.createUserObj(userName, email);
+//     console.log(userObj);
+//     API.saveToApi("user", userObj);
+//     targetContainer = "";
+//   });
+// }
 
-export { submitRegBtn, registerPageLink, loginUserBtn };
+// function registerPageLink() {
+//   document.querySelector("#register-link").addEventListener("click", () => {
+//     event.preventDefault();
+//     targetContainer.innerHTML = registerUserForm();
+//     submitRegBtn();
+//   });
+// }
+
+// function loginUserBtn() {
+//   document.querySelector("#login-btn").addEventListener("click", () => {
+//     let userName = document.querySelector("#login").value;
+//     console.log(userName);
+//   });
+// }
+
+export { EVENT };

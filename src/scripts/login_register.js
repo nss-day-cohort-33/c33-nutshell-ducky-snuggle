4a66223535cdf194ed19e06995f285b6dd3b0bd1
Shane Miller <shanemiller89@gmail.com>
2019-07-03 10:17:41 -0500

    Added password, heck yah

--- src/scripts/login_register.js
+++ src/scripts/login_register.js
@@ -8,6 +8,8 @@ return `
     <input id="userName" type="text" placeholder="ex: joeshep123">
     <label for="email">Email:</label>
     <input id="email" type="text" placeholder="ex: joeshep@hotmail.com">
+    <label for="password">Password:</label>
+    <input id="password" type="text" placeholder="ex: password123">
     <button id="submit-reg-btn">Submit</button>
 </div>
 </fieldset>
@@ -21,6 +23,8 @@ return `
 <div>
     <label for="login">Login:</label>
     <input id="login" type="text" placeholder="Your Username">
+    <label for="password">Password:</label>
+    <input id="password" type="password" placeholder="Your Password">
     <button id="login-btn">Login</button>
     <div class="register-link"><a id="register-link" href="">Register</a></div>
 </div>
--- src/scripts/utility.js
+++ src/scripts/utility.js
@@ -1,10 +1,11 @@
 //facotry functions to support build objects to API
 
 let utilityFunc = {
-    createUserObj: function (userName, email) {
+    createUserObj: function (userName, email, password) {
         return {
             user_name: userName,
-            email: email
+            email: email,
+            password: password
         }
     }
 }
